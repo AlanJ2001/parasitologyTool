f0c4d15ea5f6abef037a148bca8602e0adceb7ae
Alan Joy <2460256j@student.gla.ac.uk>
2021-11-21 16:25:11 +0000

    updated url.py

--- cs28TeamProject/parasitologyTool/urls.py
+++ cs28TeamProject/parasitologyTool/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import url
 from parasitologyTool import views
+from django.urls import path
 
 app_name = 'parasitologyTool'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about'),
-    url(r'add_post/', views.add_post, name='add_post'),
-    url(r'register/', views.register, name='register'),
-    url(r'login/', views.user_login, name='login'),
-    url(r'logout/', views.user_logout, name='logout'),
-    url(r'public_content/', views.public_content, name='public_content'),
-    url(r'add_article/',views.add_article, name='add_article'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('add_post/', views.add_post, name='add_post'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('public_content/', views.public_content, name='public_content'),
+    path('add_article/',views.add_article, name='add_article'),
 ]
