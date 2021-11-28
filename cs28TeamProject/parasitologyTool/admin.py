77bb5d71cd12f84b026ed1ba2a0461c567bfae37
Yanzhi Shu <2411021s@student.gla.ac.uk>
2021-11-28 04:11:49 +0000

    Revert "Merge branch 'dev' into 'master'"
    This reverts merge request !19

--- cs28TeamProject/parasitologyTool/admin.py
+++ cs28TeamProject/parasitologyTool/admin.py
@@ -2,15 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-
-class ParasiteAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-
 admin.site.register(Post)
 admin.site.register(UserProfile)
-<<<<<<< Updated upstream
 admin.site.register(Parasite)
 admin.site.register(Article)
-=======
-admin.site.register(Parasite, ParasiteAdmin)
->>>>>>> Stashed changes
--- cs28TeamProject/parasitologyTool/urls.py
+++ cs28TeamProject/parasitologyTool/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url
-from django.urls import path
 from parasitologyTool import views
 from django.urls import path
 
 app_name = 'parasitologyTool'
 
 urlpatterns = [
-<<<<<<< Updated upstream
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('add_post/', views.add_post, name='add_post'),
@@ -16,15 +14,4 @@ urlpatterns = [
     path('public_content/', views.public_content, name='public_content'),
     path('public_content/<int:parasite_id>/add_article/',views.add_article, name='add_article'),
     path('public_content/<int:parasite_id>/', views.public_parasite_page, name = 'public_parasite_page')
-=======
-    url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about'),
-    url(r'add_post/', views.add_post, name='add_post'),
-    url(r'register/', views.register, name='register'),
-    url(r'login/', views.user_login, name='login'),
-    url(r'logout/', views.user_logout, name='logout'),
-    path('public_content/', views.public_content, name='public_content'),
-    #url(r'add_article/',views.add_article, name='add_article'),
-    path('public_content/parasite/<slug:parasite_name_slug>/',views.show_parasite, name='show_parasite'),
->>>>>>> Stashed changes
 ]
