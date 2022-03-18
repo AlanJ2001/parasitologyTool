e4da6c9dd4f8456db4fd42797142a0cdc7bceb1b
ewan <2461447g@student.gla.ac.uk>
2022-03-18 17:41:29 +0000

    Reformatted the code to make it look nicer

--- cs28TeamProject/populate_tool.py
+++ cs28TeamProject/populate_tool.py
@@ -1,19 +1,24 @@
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cs28TeamProject.settings')
 
 import django
+
 django.setup()
 from parasitologyTool.models import Parasite
+
 
 def populate():
     parasites = ['Trypanosoma', 'Plasmodium', 'Leishmania', 'Toxoplasma', 'Helminths']
     for p in parasites:
         c = add_parasite(p)
 
+
 def add_parasite(p):
     c = Parasite.objects.get_or_create(name=p)[0]
     c.save()
     return c
+
 
 if __name__ == '__main__':
     print("populating...")
